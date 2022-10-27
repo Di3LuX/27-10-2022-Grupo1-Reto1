2582fd8f25fe93bc0729dfefbaf72405eadc1534
hector.gv00 <hector.gv00@gmail.com>
2022-10-27 12:54:31 +0200

    Añadido puerto y app.listen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const express = require ("express");
 const app = express();
+const port = 3000; 
 
 app.use(express.json) 
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`La base de datos está up y está alojada en el puerto => ${port}`);
+})
